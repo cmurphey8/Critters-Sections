74e1545ca1235021857774552271eb89d360fd2f
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2022-04-05 22:24:11 -0400

    Update Octopus.java

--- Octopus.java
+++ Octopus.java
@@ -3,54 +3,50 @@ import java.awt.Color;
 
 public class Octopus extends Critter {
 
-    // build this String with each new Critter we encounter
+    // store all nearby enemy String types
     private String nearby = "";
 
     // list of possible directions we can look for neighbors
     private final Direction[] moves = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
     
-    // always pounce
+    // WE DO: always pounce
     public Attack fight(String opponent) {
 		return Attack.POUNCE;
 	}
 
-    // always yellow
+    // WE DO: always yellow
     public Color getColor() {
         return Color.YELLOW;
     }
 
+    // WE DO: scan the area for other Critters and add to String nearby
     public Direction getMove(String[][] grid) {
         int sz = 5;
-        
         nearby = "";
         for (int i = getX() - sz; i < getX() + sz; i++){
             for (int j = getY() - sz; j < getY() + sz; j++){
-                int x = Math.floorMod(i, getWidth());
-                int y = Math.floorMod(j, getHeight());
-                if (!grid[x][y].equals(" ") 
-                        && !grid[x][y].equals(".")
-                        && !grid[x][y].equals("#")
-                    )
-                    nearby += grid[x][y]; 
+
             }
         }
+        // does not move
         return Direction.CENTER;
     }
 
-    // mimic neighbor, else pose as a random nearby Critter, else "#"
+    // WE DO: mimic neighbor, else pose as a random nearby Critter, else "#"
 	public String toString() {
-        // mimic neighbor
-        for (int i = 0; i < moves.length; i++) {
-            String tmp = getNeighbor(moves[i]);
-            if (!tmp.equals(" ")) return tmp;
-        }
+        // WE DO: if neighbor is an enemy, mimic them
 
-        // no critters nearby: relax
+        // no critters nearby -> relax
         if (nearby.equals("")) return "#";
 
-        // pose as random nearby critter
+        // WE DO: pose as random nearby critter
         Random rand = new Random();
-        int index = rand.nextInt(nearby.length());
-        return nearby.substring(index, index+1);
+        int index;
+        return "";
 	}
+
+    // WE DO: determine if String test contains an enemy Critter
+    private boolean isEnemy(String test) {
+        return false;
+    }
 }
